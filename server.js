18c2495d26ede448390623d0b2922953745c44e2
Petermuturi <pmuturi65@gmail.com>
2016-10-10 11:22:11 +0300

    added public folder

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
+var http = require ('http');
 
-app.get('/', function(req, res){
-  res.send("Hello");
+app.use(express.static(__dirname + "/public"));
+
+
+http.createServer(function(req, res){
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end(app);
 });
+
 app.listen(process.env.PORT || 3000);
